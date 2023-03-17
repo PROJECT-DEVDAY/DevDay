5b645264787379a4cd96e67744396c7e473c8afd
Dongjoon Lee <djunnni@gmail.com>
2023-03-17 16:23:38 +0900

    Feat: frontend 환경 추가

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+    'plugin:@next/next/recommended',
+  ],
+  plugins: ['prettier'],
+  // eslint-plugin-prettier를 적용시켜줍니다
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': [
+      1,
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: [
+          'arrow-function',
+          'function-declaration',
+          'function-expression',
+        ],
+      },
+    ],
+  },
+};
---
+++ frontend/src/components/SelectArrow/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import './selectoption.css';
+import { SlArrowRight } from 'react-icons/sl';
+
+export const SelectArrow = ({
+  fill,
+  className,
+  iconname,
+  title,
+  content,
+  ...props
+}) => {
+  return (
+    <div
+      className={classnames(
+        'SelectArrow',
+        className,
+        fill && 'SelectArrow-background-fill',
+      )}
+    >
+      <div style={{ flex: 1 }}>
+        <p className="arrowtitle">{title}</p>
+        <p className="arrowcontent">{content}</p>
+      </div>
+      <div className="arrowdiv">
+        <SlArrowRight className="arrow" width={30} />
+      </div>
+    </div>
+  );
+};
+
+SelectArrow.propTypes = {
+  fill: PropTypes.bool,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+SelectArrow.defaultProps = {
+  fill: true,
+  title: null,
+  content: null,
+  onClick: undefined,
+};
