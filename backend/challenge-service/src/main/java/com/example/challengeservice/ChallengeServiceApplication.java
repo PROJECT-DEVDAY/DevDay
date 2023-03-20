d815de2b9d6fc0d5c7a31405adcfcaf460f6e00d
GeumBi-Hong <canbe159@gmail.com>
2023-03-20 15:28:39 +0900

    init: challenge-service 생성

---
+++ backend/challenge-service/src/main/java/com/example/challengeservice/ChallengeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.challengeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeServiceApplication.class, args);
+    }
+
+}
