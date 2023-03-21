387d3546d0e545b8d8f65c5aadd0af5b9ff40155
daydeuk <daydeuk@naver.com>
2023-03-21 19:14:50 +0900

    Refactor: 챌린지 방 입장하기 수정

--- backend/challenge-service/src/main/java/com/example/challengeservice/repository/ChallengeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.challengeservice.repository;
-
-import org.bouncycastle.asn1.cmp.Challenge;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChallengeRepository extends JpaRepository<Challenge,Long> {
-}
