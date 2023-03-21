cdf980dd3943f613b583a5acfe175570e08aaaae
GeumBi-Hong <canbe159@gmail.com>
2023-03-21 15:14:08 +0900

    Feat: ChallengeController 추가

---
+++ backend/challenge-service/src/main/java/com/example/challengeservice/controller/ChallengeController.java
@@ -0,0 +1,20 @@
+package com.example.challengeservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/challenges")
+public class ChallengeController {
+
+    /** 챌린지 생성 **/
+
+    /** 챌린지 조회  **/
+
+    /** 챌린지 상세 조회 ** (입장 페이지)*/
+
+
+
+
+
+}
