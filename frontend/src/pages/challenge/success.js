f6d75389974f457d6fb4ffda8b688b93aec1d140
Dongjoon Lee <djunnni@gmail.com>
2023-03-21 17:03:21 +0900

    Feat: FE toss-pay example 추가

---
+++ frontend/src/pages/challenge/success.js
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div>
+      <h3>결제 성공화면</h3>
+      <p>
+        결제 성공화면입니다. 완료 후 pay-service로 confirm을 완료해주어야 하며,
+        그 때 동안 로딩화면을 유지합니다. confirm이 성공적으로 이루어지면
+        챌린지에 참여가 성공합니다.
+      </p>
+    </div>
+  );
+}
