d2ecb16c03f801b30f4f12a059353394229f0278
keeeeeey <sseioul@naver.com>
2023-03-21 16:16:06 +0900

    Feat: redis 환경설정

--- backend/user-service/src/main/java/com/example/userservice/repository/UserRepository.java
+++ backend/user-service/src/main/java/com/example/userservice/repository/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
-    Optional<User> findByNickname(String nickname);
+    Optional<User> findByNameAndNickname(String name, String nickname);
 
     Optional<User> findByNameAndNicknameAndEmail(String name, String nickname, String email);
 }
