5687de20f046fbe8e9e85fa3c2d4e6de92c06c90
hyoingwoon <cpobo@hanmail.net>
2023-03-22 15:00:31 +0900

    Feat: npm i datepicker, Add ContentInput, DayPicker, MonthPicker

---
+++ frontend/src/components/DayPicker/index.stories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import DayPicker from '.';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'DevDay/Atoms/DayPicker',
+  component: DayPicker,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = args => <DayPicker {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  date : null
+};
