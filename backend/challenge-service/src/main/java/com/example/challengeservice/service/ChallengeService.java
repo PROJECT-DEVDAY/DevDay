84ca5a8d3dafe5a9572b5b3a6c7756e63bc29aa4
daydeuk <daydeuk@naver.com>
2023-03-24 14:52:59 +0900

    FEAT : 챌린지id들로 한번에 정보 조회 추가 challengeId 리스트로 한번에 조회하는 기능 추가

--- backend/challenge-service/src/main/java/com/example/challengeservice/service/ChallengeService.java
+++ backend/challenge-service/src/main/java/com/example/challengeservice/service/ChallengeService.java
@@ -2,20 +2,19 @@ package com.example.challengeservice.service;
 
 import com.example.challengeservice.dto.request.ChallengeRecordRequestDto;
 import com.example.challengeservice.dto.request.ChallengeRoomRequestDto;
-import com.example.challengeservice.dto.response.ChallengeRoomResponseDto;
-import com.example.challengeservice.dto.response.PhotoRecordResponseDto;
-import com.example.challengeservice.dto.response.SimpleChallengeResponseDto;
-import com.example.challengeservice.dto.response.SolvedListResponseDto;
-import com.example.challengeservice.dto.response.UserChallengeInfoResponseDto;
+import com.example.challengeservice.dto.response.*;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public interface ChallengeService {
     /** 챌린지 생성 **/
     Long createChallenge(ChallengeRoomRequestDto challengeRoomRequestDto) throws IOException;
     /** 챌린지 조회 **/
     ChallengeRoomResponseDto readChallenge(Long challengeId);
+    /** 챌린지 번호들로 챌린지 정보들 조회 **/
+    Map<Long, ChallengeInfoResponseDto> challengeInfoList(List<Long> challengeIdList);
     /** 챌린지 참여 **/
     Long joinChallenge(Long challengeId, Long userId);
     /** 해당 유저의 챌린지 리스트 조회 **/
@@ -23,6 +22,8 @@ public interface ChallengeService {
     List<SimpleChallengeResponseDto> getListSimpleChallenge (String type , String search , int size , Long offset);
     /** 푼 문제 리스트 찾기 **/
     SolvedListResponseDto solvedProblemList(String baekjoonId);
+    /** 유저의 백준리스트  **/
+    // updateUserBaekjoon(String baekjoonId, String userId);
 
     /** 사진 인증 기록 생성하기 **/
     void createPhotoRecord (ChallengeRecordRequestDto requestDto) throws IOException;
