835785c4409f336547af96398fa7df52cb36502f
박태환 <parkssuu@gmail.com>
2023-03-25 01:47:12 +0900

    Fix: css selector name update

---
+++ frontend/src/components/MainHeader/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+export const MainHeader = props => {
+  return <div>index</div>;
+};
+
+MainHeader.propTypes = {};
