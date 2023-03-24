ec2ad10e89088b808f0c677d83afdb494e070d10
keeeeeey <sseioul@naver.com>
2023-03-24 16:51:40 +0900

    Fix: 백준 문제 Response 타입 변경

--- backend/user-service/src/main/java/com/example/userservice/dto/response/BaekjoonListResponseDto.java
+++ backend/user-service/src/main/java/com/example/userservice/dto/response/BaekjoonListResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Getter
@@ -15,5 +16,12 @@ public class BaekjoonListResponseDto {
 
     private String baekjoonId;
 
-    private List<String> problemList;
+    private HashMap<String, String> problemList;
+
+    public static BaekjoonListResponseDto of (String baekjoonId, HashMap<String, String> problemList) {
+        return BaekjoonListResponseDto.builder()
+                .baekjoonId(baekjoonId)
+                .problemList(problemList)
+                .build();
+    }
 }
