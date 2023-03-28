007843bbb438a929414f1416c21e53f1fb27d76e
keeeeeey <sseioul@naver.com>
2023-03-28 12:07:05 +0900

    FEAT: 닉네임 중복검사 기능 추가

---
+++ backend/user-service/src/main/java/com/example/userservice/dto/request/NicknameCheckRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.userservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NicknameCheckRequestDto {
+
+    private String nickname;
+}
