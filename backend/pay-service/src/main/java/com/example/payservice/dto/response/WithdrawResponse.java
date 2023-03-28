b0907152e4060389a67600a097571f2dd232b956
Dongjoon Lee <djunnni@gmail.com>
2023-03-28 11:59:21 +0900

    Fix: 출금 response dto에서 builder패턴 적용

--- backend/pay-service/src/main/java/com/example/payservice/dto/response/WithdrawResponse.java
+++ backend/pay-service/src/main/java/com/example/payservice/dto/response/WithdrawResponse.java
@@ -3,10 +3,12 @@ package com.example.payservice.dto.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
