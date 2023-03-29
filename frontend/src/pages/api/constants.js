6609d768943476f81ed66cbb4e71143952dcc28f
pthwan27 <parkssuu@gmail.com>
2023-03-29 10:35:25 +0900

    Feat: npm i axios

---
+++ frontend/src/pages/api/constants.js
@@ -0,0 +1,20 @@
+// 유저
+const USER_SERVICE = 'user-service';
+
+export const JOIN_URL = param => `${USER_SERVICE}/join/${param}`;
+
+export const LOGIN_URL = `${USER_SERVICE}/login`;
+export const EMAIL_URL = `${USER_SERVICE}/email`;
+export const CONFIRM_EMAIL_URL = `${USER_SERVICE}/confirm-email`;
+
+// 챌린지
+const CHALLENGE_SERVICE = 'challenge-service';
+
+export const CHALLENGES_URL = `${CHALLENGE_SERVICE}/challenges`;
+export const CHALLENGES_LIST_URL = param =>
+  `${CHALLENGE_SERVICE}/challenges/list?category=${param}`;
+
+// 결제
+const PAY_SERVICE = 'pay-service';
+export const PAYMENT_CHALLENGE_SUCCESS = param =>
+  `${PAY_SERVICE}/${param}/success`;
