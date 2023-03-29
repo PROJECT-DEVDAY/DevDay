9ff1f4e753b71652803d55a53772d58253a4225a
keeeeeey <sseioul@naver.com>
2023-03-29 15:11:39 +0900

    Fix: 백준 아이디 업데이트 로직 수정

--- backend/user-service/src/main/java/com/example/userservice/client/ChallengeServiceClient.java
+++ backend/user-service/src/main/java/com/example/userservice/client/ChallengeServiceClient.java
@@ -6,7 +6,6 @@ import com.example.userservice.dto.response.ProblemResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "challenge-service")
 public interface ChallengeServiceClient {
