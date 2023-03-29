2637dbcee45606e0149058c55e0a2a868f8b009a
신대득 <daydeuk@naver.com>
2023-03-29 15:57:16 +0900

    Test : gateway ip 테스트

--- backend/user-service/src/main/java/com/example/userservice/config/SecurityConfig.java
+++ backend/user-service/src/main/java/com/example/userservice/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        String IpAddress = env.getProperty("gateway.ip");
+       String IpAddress = env.getProperty("gateway.ip");
 
         http
                 .httpBasic().disable()
@@ -34,8 +34,8 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .antMatchers("/error/**").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
-//                .antMatchers("/**")
-//                .access("hasIpAddress('" + IpAddress + "')")
+               .antMatchers("/**")
+               .access("hasIpAddress('" + IpAddress + "')")
                 .and()
                 .headers().frameOptions().disable();
         return http.build();
