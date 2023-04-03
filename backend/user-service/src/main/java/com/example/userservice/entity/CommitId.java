2b459df570743a1770e152feb623f25b47e1293d
keeeeeey <sseioul@naver.com>
2023-04-03 10:28:23 +0900

    Feat: 커밋 기록 생성, 업데이트, 조회

---
+++ backend/user-service/src/main/java/com/example/userservice/entity/CommitId.java
@@ -0,0 +1,18 @@
+package com.example.userservice.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class CommitId implements Serializable {
+
+    private String commitDate;
+
+    private Long user;
+}
---
+++ backend/user-service/src/main/java/com/example/userservice/repository/CommitRecordRepository.java
@@ -0,0 +1,12 @@
+package com.example.userservice.repository;
+
+import com.example.userservice.entity.CommitId;
+import com.example.userservice.entity.CommitRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CommitRecordRepository extends JpaRepository<CommitRecord, CommitId> {
+
+    Optional<CommitRecord> findByCommitDateAndUserId(String commitDate, Long userId);
+}
