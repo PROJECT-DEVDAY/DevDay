d37e7eb47b8938f87b9074af61c84204540f4e99
keeeeeey <sseioul@naver.com>
2023-04-04 10:09:18 +0900

    Fix: deleteUser

--- backend/user-service/src/main/java/com/example/userservice/service/AuthServiceImpl.java
+++ backend/user-service/src/main/java/com/example/userservice/service/AuthServiceImpl.java
@@ -57,11 +57,9 @@ public class AuthServiceImpl implements AuthService {
 
         // 푼 백준 문제 삭제
         solvedacRepository.deleteAllByUserId(user.getId());
-        em.flush();
 
         // 커밋 기록 삭제
         commitRecordRepository.deleteAllByUserId(user.getId());
-        em.flush();
 
         // 회원 탈퇴
         userRepository.delete(user);
@@ -95,7 +93,7 @@ public class AuthServiceImpl implements AuthService {
     }
 
     private void deleteS3Img(User user) {
-        if (!user.getProfileImgKey().isBlank()) amazonS3Service.delete(user.getProfileImgKey());
+        if (user.getProfileImgKey() != null && !user.getProfileImgKey().isBlank()) amazonS3Service.delete(user.getProfileImgKey());
     }
 
     private String saveS3Img(MultipartFile profileImg) {
@@ -140,7 +138,7 @@ public class AuthServiceImpl implements AuthService {
 
         solvedacRepository.deleteAllByUserId(user.getId());
         em.flush();
-        if (!user.getBaekjoon().isBlank()) commonService.saveProblemList(user);
+        if (user.getBaekjoon() != null && !user.getBaekjoon().isBlank()) commonService.saveProblemList(user);
 
         return GithubBaekjoonResponseDto.from(user.getGithub(), user.getBaekjoon());
     }
