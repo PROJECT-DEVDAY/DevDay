7cec3e2fec69f15112c36630a423c9691db2e37b
Dongjoon Lee <djunnni@gmail.com>
2023-04-05 11:38:27 +0900

    Fix: 날짜 계산시 1달 전으로 잡히는 버그 수정

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -5,10 +5,10 @@ const DAY = ['일', '월', '화', '수', '목', '금', '토'];
 export const getStartWithEndDate = (startDate, endDate) => {
   const start = new Date(startDate);
   const end = new Date(endDate);
-  const startMonth = `${start.getMonth()}`.padStart(2, '0');
+  const startMonth = `${start.getMonth() + 1}`.padStart(2, '0');
   const startDay = `${start.getDate()}`.padStart(2, '0');
 
-  const endMonth = `${end.getMonth()}`.padStart(2, '0');
+  const endMonth = `${end.getMonth() + 1}`.padStart(2, '0');
   const endDay = `${end.getDate()}`.padStart(2, '0');
 
   return `${startMonth}.${startDay}(${
