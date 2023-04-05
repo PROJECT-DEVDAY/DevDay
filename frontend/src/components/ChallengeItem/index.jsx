5a96c423a13834e79707d43e551fcaa1610f2b33
Dongjoon Lee <djunnni@gmail.com>
2023-04-05 22:53:34 +0900

    Fix: 메인화면 호스트 닉네임 수정

--- frontend/src/components/ChallengeItem/index.jsx
+++ frontend/src/components/ChallengeItem/index.jsx
@@ -1,81 +1,71 @@
-import React, { useState, forwardRef } from 'react';
+import React, { useState } from 'react';
 
 import classNames from 'classnames';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-// import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 
 import style from './index.module.scss';
 
-export const ChallengeItem = forwardRef(
-  (
-    {
-      className,
-      id,
-      imgUrl,
-      participants,
-      leader,
-      title,
-      period,
-      onClick,
-      ...props
-    },
-    ref,
-  ) => {
-    const router = useRouter();
-
-    return (
-      <Link
-        ref={ref}
-        href={`participation/${id}`}
-        className={style.ChallengeItem}
-        role="button"
-        tabIndex={0}
+export const ChallengeItem = ({
+  className,
+  id,
+  imgUrl,
+  participants,
+  leader,
+  title,
+  period,
+  onClick,
+  ...props
+}) => {
+  return (
+    <Link
+      href={`participation/${id}`}
+      className={style.ChallengeItem}
+      role="button"
+      tabIndex={0}
+    >
+      <div
+        className={classNames(style.ImageContainer, `relative object-cover`)}
       >
+        <img
+          className={classNames(style.Image, `rounded-lg`)}
+          src={imgUrl || require('../../image/default-user.png')}
+          alt="temp"
+        />
         <div
-          className={classNames(style.ImageContainer, `relative object-cover`)}
+          className={classNames(
+            style.Participants,
+            `absolute rounded-md p-1 font-bold text-xs`,
+          )}
         >
-          <img
-            className={classNames(style.Image, `rounded-lg`)}
-            src={imgUrl || require('../../image/default-user.png')}
-            alt="temp"
-          />
-          <div
-            className={classNames(
-              style.Participants,
-              `absolute rounded-md p-1 font-bold text-xs`,
-            )}
-          >
-            {participants}명
-          </div>
+          {participants}명
         </div>
-        <div className={classNames(`flex flex-col ml-1 pt-1`)}>
-          <div className={classNames(style.Leader, `font-medium text-xs`)}>
-            {leader}
-          </div>
-          <div
-            className={classNames(
-              style.Title,
-              `font-bold text-base overflow-hidden text-ellipsis whitespace-nowrap`,
-            )}
-          >
-            {title}
-          </div>
-          <div className={classNames(style.Period, `font-medium text-xs`)}>
-            {period}
-          </div>
+      </div>
+      <div className={classNames(`flex flex-col ml-1 pt-1`)}>
+        <div className={classNames(style.Leader, `font-medium text-xs`)}>
+          {leader}
         </div>
-      </Link>
-    );
-  },
-);
+        <div
+          className={classNames(
+            style.Title,
+            `font-bold text-base overflow-hidden text-ellipsis whitespace-nowrap`,
+          )}
+        >
+          {title}
+        </div>
+        <div className={classNames(style.Period, `font-medium text-xs`)}>
+          {period}
+        </div>
+      </div>
+    </Link>
+  );
+};
 
 ChallengeItem.propTypes = {
   imageUrl: PropTypes.string,
   participants: PropTypes.number.isRequired,
-  leader: PropTypes.string.isRequired,
+  leader: PropTypes.string,
   title: PropTypes.string.isRequired,
   period: PropTypes.string.isRequired,
   onClick: PropTypes.func,
