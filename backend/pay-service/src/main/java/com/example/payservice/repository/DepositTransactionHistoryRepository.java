cebaaca1eda7b2825acaf1fafb5ae943b985ef13
Dongjoon Lee <djunnni@gmail.com>
2023-04-06 21:26:24 +0900

    Feat: 예치금 Summary API 제작

--- backend/pay-service/src/main/java/com/example/payservice/repository/DepositTransactionHistoryRepository.java
+++ backend/pay-service/src/main/java/com/example/payservice/repository/DepositTransactionHistoryRepository.java
@@ -1,6 +1,7 @@
 package com.example.payservice.repository;
 
 import com.example.payservice.dto.deposit.DepositTransactionType;
+import com.example.payservice.entity.DepositSummary;
 import com.example.payservice.entity.DepositTransactionHistoryEntity;
 import com.example.payservice.entity.PayUserEntity;
 import org.springframework.data.domain.Page;
@@ -9,7 +10,6 @@ import org.springframework.data.jpa.repository.*;
 
 import javax.persistence.LockModeType;
 import javax.persistence.QueryHint;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -29,4 +29,20 @@ public interface DepositTransactionHistoryRepository extends JpaRepository<Depos
     @Modifying
     @Query("DELETE FROM DepositTransactionHistoryEntity p where p.user = :user")
     void deleteAllByUser(PayUserEntity user);
+
+    @Query("SELECT p.type as depositTransactionType, SUM(p.amount) as sum FROM DepositTransactionHistoryEntity p WHERE p.user = :user GROUP BY p.type")
+    List<DepositSummary> getSummaryInfoByUserAndType(PayUserEntity user);
+
+    @Query("SELECT p.challengeId " +
+            "FROM DepositTransactionHistoryEntity p " +
+            "WHERE p.user = :user and p.type in :types " +
+            "GROUP BY p.challengeId, p.user " +
+            "HAVING COUNT(*) >= 2")
+    List<Long> getDoneChallengesByUser(PayUserEntity user, DepositTransactionType[] types);
+
+    @Query("SELECT coalesce(SUM(p.amount), 0) " +
+            "FROM DepositTransactionHistoryEntity p " +
+            "WHERE p.user = :user and p.challengeId not in :challengeIds and p.type = :type"
+    )
+    Integer getCurrentChallengingAmountByUser(PayUserEntity user, List<Long> challengeIds, DepositTransactionType type);
 }
