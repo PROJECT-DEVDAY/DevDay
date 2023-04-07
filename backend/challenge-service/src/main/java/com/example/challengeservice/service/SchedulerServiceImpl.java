1b61c958c86c65e6139feee1bc5cab89f134d4b2
canbe159 <canbe159@gmail.com>
2023-04-07 10:25:07 +0900

    Fix: pay-service로 최종 정산 로직 수정

--- backend/challenge-service/src/main/java/com/example/challengeservice/service/SchedulerServiceImpl.java
+++ backend/challenge-service/src/main/java/com/example/challengeservice/service/SchedulerServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.challengeservice.service;
 
 import com.example.challengeservice.client.PayServiceClient;
+import com.example.challengeservice.client.dto.ChallengeSettleInfo;
 import com.example.challengeservice.client.dto.ChallengeSettlementRequest;
 import com.example.challengeservice.repository.ChallengeRoomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +22,8 @@ public class SchedulerServiceImpl implements SchedulerService{
     private final  CommonService commonService;
     @Override
     public void endChallengeCalculate() {
+
+
       payServiceClient.requestChallengeSettlement(new ChallengeSettlementRequest(challengeRoomRepository.findClosedChallengeUser(commonService.getPastDay(0,commonService.getDate()))));
     }
 }
